54206577df4838b2b15f53a4eb31419daea62d0e
ezaksch <akshaychauhanjp@gmail.com>
2020-02-13 18:17:57 +0530

    ServiceRegistry has been defined

--- src/main/java/com/sample/hibernate/hibernate_telesco/App.java
+++ src/main/java/com/sample/hibernate/hibernate_telesco/App.java
@@ -1,9 +1,12 @@
 package com.sample.hibernate.hibernate_telesco;
 
+import org.hibernate.service.ServiceRegistry;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  * Hello world!
@@ -14,13 +17,15 @@ public class App
     public static void main( String[] args )
     {
         Alien alien=new Alien();
-        alien.setaId(1);
-        alien.setaName("Akshay");
-        alien.setColor("blue");
+        alien.setaId(2);
+        alien.setaName("SomeOne");
+        alien.setColor("Orange");
         
         Configuration con =new Configuration().configure().addAnnotatedClass(Alien.class);
         
-        SessionFactory sf=con.buildSessionFactory();
+        ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+        
+        SessionFactory sf=con.buildSessionFactory(reg);
         
         Session session=sf.openSession();
         
