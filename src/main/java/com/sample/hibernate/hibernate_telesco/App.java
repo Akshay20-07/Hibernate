325a33c658ee8d34aff7381ada49818038d24df9
ezaksch <akshaychauhanjp@gmail.com>
2020-02-13 20:18:00 +0530

    Documentation till EMBEDDABLE

--- src/main/java/com/sample/hibernate/hibernate_telesco/App.java
+++ src/main/java/com/sample/hibernate/hibernate_telesco/App.java
@@ -12,38 +12,41 @@ import org.hibernate.service.ServiceRegistryBuilder;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	AlienName aName=new AlienName();
-    	aName.setfName("Akshay");
-    	aName.setmName("Singh");
-    	aName.setlName("Chauhan");
-    	
-        Alien alien=new Alien();
-        alien.setaId(2);
-        alien.setColor("Orange");
-        alien.setalienName(aName);
-        
-        Configuration con =new Configuration().configure().addAnnotatedClass(Alien.class);
-        
-        ServiceRegistry reg= new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
-        
-        SessionFactory sf=con.buildSessionFactory(reg);
-        
-        Session session=sf.openSession();
-        
-        Transaction txn=session.beginTransaction();
-        session.save(alien);
-        /**
-         * For Fetching the Values
-         */
-        //alien=(Alien) session.get(Alien.class,3);
-        
-        txn.commit();
-        
-        System.out.println(alien);
-        
-    }
+public class App {
+	/**
+	 * For simple hibernate connection
+	 * fetching and Saving the name plus Use of Embeddable
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AlienName aName = new AlienName();
+		aName.setfName("Akshay");
+		aName.setmName("Singh");
+		aName.setlName("Chauhan");
+
+		Alien alien = new Alien();
+		alien.setaId(2);
+		alien.setColor("Orange");
+		alien.setalienName(aName);
+
+		Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+
+		ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+
+		SessionFactory sf = con.buildSessionFactory(reg);
+
+		Session session = sf.openSession();
+
+		Transaction txn = session.beginTransaction();
+		session.save(alien);
+		/**
+		 * For Fetching the Values
+		 */
+		// alien=(Alien) session.get(Alien.class,3);
+
+		txn.commit();
+
+		System.out.println(alien);
+
+	}
 }
