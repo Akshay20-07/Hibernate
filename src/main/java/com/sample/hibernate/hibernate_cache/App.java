3c410825644960a8e1e5ad2e86f21b2d9dfe6a7e
ezaksch <akshaychauhanjp@gmail.com>
2020-02-17 14:32:58 +0530

    Hibernate query for Cache

--- src/main/java/com/sample/hibernate/hibernate_cache/App.java
+++ src/main/java/com/sample/hibernate/hibernate_cache/App.java
@@ -1,5 +1,6 @@
 package com.sample.hibernate.hibernate_cache;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,7 +8,7 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 public class App {
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		
 		AlienCache a=null;
 		
@@ -25,6 +26,33 @@ public class App {
 		Session session2=sf.openSession();
 		session2.beginTransaction();
 		a=(AlienCache) session2.get(AlienCache.class, 2);
+		System.out.println(a.toString());
+		
+		session2.getTransaction().commit();
+		session2.close();
+	}*/
+	
+	public static void main(String[] args) {
+		
+		AlienCache a=null;
+		
+		Configuration con= new Configuration().configure("/hibernate.cfg.xml").addAnnotatedClass(AlienCache.class);
+		ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+		SessionFactory sf=con.buildSessionFactory(reg);
+		Session session1=sf.openSession();
+		session1.beginTransaction();
+		Query q1=session1.createQuery("from AlienCache where aid=2");
+		q1.setCacheable(true);
+		a=(AlienCache)q1.uniqueResult();
+		System.out.println(a.toString());
+		session1.getTransaction().commit();
+		session1.close();
+		
+		Session session2=sf.openSession();
+		session2.beginTransaction();
+		Query q2=session2.createQuery("from AlienCache where aid=2");
+		q2.setCacheable(true);
+		a=(AlienCache)q2.uniqueResult();
 		System.out.println(a.toString());
 		
 		session2.getTransaction().commit();
